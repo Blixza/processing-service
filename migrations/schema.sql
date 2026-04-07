fa8a27a5343c3f7f163168edac7a95139770480a
Blixza <slava.repin10@mail.ru>
2026-04-07 20:39:03 +0500

    fix: add columns to schema.sql

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -3,6 +3,8 @@ CREATE TABLE IF NOT EXISTS jobs (
     type VARCHAR(50) NOT NULL,
     status VARCHAR(20) NOT NULL,
     payload TEXT,
+    filename TEXT,
+    callback_url TEXT,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
